a3f55c8a8ebdf8b6be18a00c1c0423f5a6f9d8ef
Arno Volts <voltsnntanisimine@gmail.com>
2023-11-06 17:32:22 +0100

    Create home route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+use std::net::SocketAddr;
+use axum::{
+    routing::get,
+    Router,
+};
+use crate::handlers::index;
+
+#[tokio::main]
+async fn main() {
+    // Define router
+    let app = Router::new().route("/", get(index));
+
+    // Define server address and listening port
+    let server_addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    println!("->> Running server on {}", server_addr);
+    axum::Server::bind(&server_addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
