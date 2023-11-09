0b5fa53993096a1898442dd66a0cfbf63a17120e
Arno Volts <voltsnntanisimine@gmail.com>
2023-11-09 01:46:31 +0100

    Create about route

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 mod handlers;
+mod utils;
+
 use std::net::SocketAddr;
 use axum::{
     routing::get,
     Router,
 };
-use crate::handlers::index;
+use crate::handlers::{about, index};
 
 #[tokio::main]
 async fn main() {
     // Define router
-    let app = Router::new().route("/", get(index));
+    let app = Router::new()
+        .route("/about", get(about))
+        .route("/", get(index));
 
     // Define server address and listening port
     let server_addr = SocketAddr::from(([127, 0, 0, 1], 3000));
