65538d06094de54eb651337d6c429aee189d1f2e
Arno Volts <voltsnntanisimine@gmail.com>
2023-11-09 01:45:17 +0100

    Make struct and fn public

--- src/handlers/index.rs
+++ src/handlers/index.rs
@@ -1,12 +1,9 @@
-use axum::response::{Html, IntoResponse, Response};
+use crate::utils::html_template::*;
 use askama::Template;
+
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate<'a> {
-    name: &'a str,
-}
-pub async fn index() -> Response {
-    let hello = IndexTemplate{name: "world"};
-
-    Html(hello.render().unwrap()).into_response()
+pub struct IndexTemplate;
+pub async fn index() -> HtmlTemplate<IndexTemplate>{
+    HtmlTemplate(IndexTemplate)
 }
